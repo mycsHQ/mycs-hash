96acf8c47fed01700950fe0a089699a89f79df2b
Claudio Bredfeldt <claudio@cbmono.de>
2017-03-03 15:50:24 +0100

    feat: Add Sydetable JSON schema validation [MYCS-5576] (#11)

--- src/mycs-hash-design.js
+++ src/mycs-hash-design.js
@@ -1,40 +1,46 @@
-//
-// External dependencies
-//
+/**
+ * External dependencies
+ */
 const jsSHA = require('jssha');
 const stringifier = require('./stringify');
 const V = require('jsonschema').Validator;
-const shelfSchema = require('./json-schemas/shelf.json');
-const couchtableSchema = require('./json-schemas/couchtable.json');
-const tableSchema = require('./json-schemas/table.json');
-const sydetableSchema = require('./json-schemas/sydetable.json');
-const wardrobeSchema = require('./json-schemas/wardrobe.json');
 
 const validator = new V();
 
-// should be incremented when releasing a new version
+const couchtableSchema = require('./json-schemas/couchtable.json');
+const shelfSchema = require('./json-schemas/shelf.json');
+const sydetableSchema = require('./json-schemas/sydetable.json');
+const tableSchema = require('./json-schemas/table.json');
+const wardrobeSchema = require('./json-schemas/wardrobe.json');
+
+/**
+ * Should be incremented when releasing a new version
+ */
 const HASH_ALGORITHM = 'SHA-1';
 
-//
-// WARNING !!
-// Change this key to easily seed new collection of hashes for same set of structures
-// It is recommended to make the version of the lib evolves when you do so
-// A consequence is also that the matching with persisted hash (using different lib version) will not work
-//
+/**
+ * WARNING !!
+ * Change this key to easily seed new collection of hashes for same set of structures
+ * It is recommended to make the version of the lib evolves when you do so
+ * A consequence is also that the matching with persisted hash (using different lib version)
+ * will not work
+ */
 const HMAC_KEY = '0111201600';
 
-//
-// Clone deep
-//
-// @param {object} obj
-//
+/**
+ * Clone deep
+ *
+ * @param {object} obj
+ * @returns {object}
+ */
 const _cloneDeep = obj => JSON.parse(JSON.stringify(obj));
 
-//
-// Check whether the data structure passes the criteria to be further hashed
-//
-// @param {object} deserialized json object representing a piece of furniture
-//
+/**
+ * Check whether the data structure passes the criteria to be further hashed
+ *
+ * @param {object} data json object representing a piece of furniture
+ * @returns {true|error}
+ */
 const _validateInput = function (data) {
   if (!data.structure) {
     throw new Error('missing structure attribute');
@@ -47,40 +53,49 @@ const _validateInput = function (data) {
   return _validateStructure(data.structure);
 };
 
-//
-// Validate structure. Json-schema validation
-//
-// @param {object} structure
-//
+/**
+ * Validate structure. Json-schema validation
+ *
+ * @param {object} structure
+ * @returns {true|error}
+ */
 var _validateStructure = function (structure) {
-  const shelfRes = validator.validate(structure, shelfSchema);
   const couchtableRes = validator.validate(structure, couchtableSchema);
-  const tableRes = validator.validate(structure, tableSchema);
+  const shelfRes = validator.validate(structure, shelfSchema);
   const sydetableRes = validator.validate(structure, sydetableSchema);
+  const tableRes = validator.validate(structure, tableSchema);
   const wardrobeRes = validator.validate(structure, wardrobeSchema);
 
-  if (shelfRes.errors.length && couchtableRes.errors.length && tableRes.errors.length && wardrobeRes.errors.length && sydetableRes.errors.length) {
+  if (couchtableRes.errors.length
+   && shelfRes.errors.length
+   && sydetableRes.errors.length
+   && tableRes.errors.length
+   && wardrobeRes.errors.length) {
     const error = new Error('structure is invalid for any existing json-schema');
+
     error.data = {
       structure,
       schemas: {
-        shelf: shelfRes.errors,
         couchtable: couchtableRes.errors,
-        table: tableRes.errors,
+        shelf: shelfRes.errors,
         sydetable: sydetableRes.errors,
+        table: tableRes.errors,
         wardrobe: wardrobeRes.errors
       }
     };
 
     throw error;
   }
+
+  return true;
 };
 
-//
-// Hashing function
-//
-// @param {object} data deserialized json object representing a piece of furniture
-//
+/**
+ * Hashing function
+ *
+ * @param {object} data Deserialized JSON obj representing a piece of furniture
+ * @returns {string}
+ */
 const hashingFunction = function (data) {
   data = _cloneDeep(data);
 
@@ -92,6 +107,7 @@ const hashingFunction = function (data) {
   const shaObj = new jsSHA(HASH_ALGORITHM, 'TEXT');
   shaObj.setHMACKey(HMAC_KEY, 'TEXT');
   shaObj.update(stringToHash);
+
   return shaObj.getHMAC('HEX');
 };
 
