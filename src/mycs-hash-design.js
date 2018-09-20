1b1762a54baa8296dc9159000bc2d3d6720200a8
frederic <frederic.desmarescaux@gmail.com>
2018-09-20 15:59:48 +0200

    feat: remove validation

--- src/mycs-hash-design.js
+++ src/mycs-hash-design.js
@@ -3,16 +3,6 @@
  */
 const jsSHA = require('jssha');
 const stringifier = require('./stringify');
-const V = require('jsonschema').Validator;
-
-const validator = new V();
-
-const couchtableSchema = require('./json-schemas/couchtable.json');
-const genericSchema = require('./json-schemas/generic-structure.json');
-const shelfSchema = require('./json-schemas/shelf.json');
-const sydetableSchema = require('./json-schemas/sydetable.json');
-const tableSchema = require('./json-schemas/table.json');
-const wardrobeSchema = require('./json-schemas/wardrobe.json');
 
 /**
  * Should be incremented when releasing a new version
@@ -37,64 +27,6 @@ const HMAC_KEY = '0111201600';
 const _cloneDeep = obj => JSON.parse(JSON.stringify(obj));
 
 /**
- * Check whether the data structure passes the criteria to be further hashed
- *
- * @param {object} data json object representing a piece of furniture
- * @returns {true|error}
- */
-const _validateInput = function (data) {
-  if (!data.structure) {
-    throw new Error('missing structure attribute');
-  }
-
-  if (Object.keys(data).length !== 1) {
-    throw new Error('there must be structure attribute only');
-  }
-
-  return _validateStructure(data.structure);
-};
-
-/**
- * Validate structure. Json-schema validation
- *
- * @param {object} structure
- * @returns {true|error}
- */
-var _validateStructure = function (structure) {
-  const couchtableRes = validator.validate(structure, couchtableSchema);
-  const genericRes = validator.validate(structure, genericSchema);
-  const shelfRes = validator.validate(structure, shelfSchema);
-  const sydetableRes = validator.validate(structure, sydetableSchema);
-  const tableRes = validator.validate(structure, tableSchema);
-  const wardrobeRes = validator.validate(structure, wardrobeSchema);
-
-  if (couchtableRes.errors.length
-   && genericRes.errors.length
-   && shelfRes.errors.length
-   && sydetableRes.errors.length
-   && tableRes.errors.length
-   && wardrobeRes.errors.length) {
-    const error = new Error('structure is invalid for any existing json-schema');
-
-    error.data = {
-      structure,
-      schemas: {
-        couchtable: couchtableRes.errors,
-        generic: genericRes.errors,
-        shelf: shelfRes.errors,
-        sydetable: sydetableRes.errors,
-        table: tableRes.errors,
-        wardrobe: wardrobeRes.errors
-      }
-    };
-
-    throw error;
-  }
-
-  return true;
-};
-
-/**
  * Hashing function
  *
  * @param {object} data Deserialized JSON obj representing a piece of furniture
@@ -102,8 +34,6 @@ var _validateStructure = function (structure) {
  */
 const hashingFunction = function (data) {
   data = _cloneDeep(data);
-
-  _validateInput(data);
 
   // produce the serialized json to be hashed
   const stringToHash = stringifier.stringify(data);
--- tests/sydetable-design.spec.js
+++ tests/sydetable-design.spec.js
@@ -4,35 +4,6 @@ import sydetable from './structures/sydetable';
 
 
 describe('Sydetable with mycs-hash-design', () => {
-  const testException = function (input, keyWords, done) {
-    try {
-      hashDesign(input);
-    } catch (e) {
-      expect(e.message.indexOf(keyWords) >= 0).toBe(true);
-      done();
-
-      return
-    }
-
-    throw new Error(`should be throw Exception: ${ keyWords }`);
-  };
-
-  it('should not accept input without structure', done => {
-    const input = {};
-    testException(input, 'missing structure', done);
-  });
-
-  it('should not accept input with invalid attributes', done => {
-    const input = _.pick(sydetable, [ 'structure', 'camera', 'quality' ]);
-    testException(input, 'structure attribute only', done);
-  });
-
-  it('should not accept input with invalid structure', done => {
-    const input = _.cloneDeep(_.pick(sydetable, [ 'structure' ]));
-    input.structure.field = {};
-    testException(input, 'structure is invalid', done);
-  });
-
   it('should accept input with valid structure', () => {
     const input = _.cloneDeep(_.pick(sydetable, [ 'structure' ]));
     expect(hashDesign(input)).toEqual('968e9833e8ce68fcd11aff7aff91867fbe66a125');
