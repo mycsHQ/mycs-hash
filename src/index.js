0fbf73079654259d3b2691742394ed67420230b1
Valentyn Kalinchak <valentyn@mycs.com>
2019-08-23 15:35:44 +0200

    feat: expose stringifier interface

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   hashImage: require('./mycs-hash-image'),
   hashDesign: require('./mycs-hash-design'),
-  sha256: require('./sha256')
+  sha256: require('./sha256'),
+  stringifier: require('./stringify')
 };
